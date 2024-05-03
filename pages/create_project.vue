df2b3274c0822a67d7606ea4fc07e78f84054a23
Notthingknow <cc20030804@gmail.com>
2024-05-03 11:37:49 +0800

    Draft

---
+++ pages/create_project.vue
@@ -0,0 +1,80 @@
+<script setup>
+</script>
+<template>
+    <div class="w-[1334px] h-[1713px] flex items-center justify-center">
+        <div class="w-[180px] h-[60px] left-[1154px] top-[1653px] absolute">
+            <div class="w-[180px] h-[60px] left-0 top-0 absolute bg-white rounded-[15px]"></div>
+            <div class="w-16 h-[37px] left-[58px] top-[11px] absolute text-black text-[32px] font-bold font-['Inter']">
+                發布</div>
+        </div>
+        <div class="w-[843.06px] h-[60px] left-[20px] top-[270px] absolute">
+            <div class="w-[296.06px] h-14 left-[547px] top-[2px] absolute">
+                <div class="w-[237.06px] left-[59px] top-0 absolute text-white text-[46px] font-light font-['Inter']">
+                    需指導者</div>
+                <div class="w-[45px] h-[45px] left-0 top-[5px] absolute border-2 border-violet-400"></div>
+            </div>
+            <div class="w-[242px] h-[60px] left-0 top-0 absolute">
+                <div class="w-[210.51px] left-0 top-[11px] absolute text-white text-[32px] font-light font-['Inter']">
+                    申請人數</div>
+                <div class="w-[81px] h-[60px] left-[161px] top-0 absolute">
+                    <div class="w-[81px] h-[60px] left-0 top-0 absolute bg-white/opacity-20 rounded-[15px]"></div>
+                    <div
+                        class="w-[34px] left-[31px] top-[7px] absolute text-white text-[38px] font-normal font-['Inter']">
+                        1</div>
+                </div>
+            </div>
+        </div>
+        <div class="w-[1334px] h-[90px] left-0 top-[127px] absolute">
+            <div class="w-[1334px] h-[90px] left-0 top-0 absolute bg-neutral-800 rounded-[5px]"></div>
+            <div
+                class="w-[167.12px] left-[19.78px] top-[26px] absolute text-white text-[32px] font-light font-['Inter']">
+                標題</div>
+        </div>
+        <div class="w-[373px] h-[41px] left-[20px] top-[383px] absolute">
+            <div
+                class="w-[210.51px] h-[3.16px] left-0 top-[1px] absolute text-white text-[32px] font-light font-['Inter']">
+                畢業專題</div>
+            <div class="w-[187px] h-[4.16px] left-[186px] top-0 absolute">
+                <div class="w-8 h-[3.16px] left-0 top-[1px] absolute text-white text-[32px] font-light font-['Inter']">是
+                </div>
+                <div
+                    class="w-8 h-[3.16px] left-[155px] top-0 absolute text-white text-[32px] font-light font-['Inter']">
+                    否</div>
+            </div>
+        </div>
+        <div class="w-[1334px] h-[408px] left-0 top-[583px] absolute">
+            <div class="w-[1334px] h-[408px] left-0 top-0 absolute bg-neutral-800 rounded-[5px]"></div>
+            <div class="w-[84px] left-[27px] top-[26px] absolute text-white text-[32px] font-light font-['Inter']">說明
+            </div>
+        </div>
+        <div class="w-[1331px] h-[536px] left-[3px] top-[1020px] absolute">
+            <div class="w-[1331px] h-[536px] left-0 top-0 absolute bg-violet-400/opacity-50"></div>
+            <div
+                class="w-[918px] h-[73px] left-[207px] top-[174px] absolute text-center text-violet-400 text-6xl font-bold font-['Inter']">
+                Choose files or drag and drop</div>
+            <div
+                class="w-[392px] h-12 left-[452px] top-[276px] absolute text-center text-white text-[40px] font-semibold font-['Inter']">
+                Allowed content</div>
+            <div class="w-[366.37px] h-[99px] left-[477.44px] top-[399px] absolute bg-violet-400 rounded-[20px]"></div>
+            <div
+                class="w-[220px] h-[74px] left-[556px] top-[412px] absolute text-center text-white text-6xl font-semibold font-['Inter']">
+                Ready</div>
+            <div class="w-[93.54px] h-24 left-[618.73px] top-[49px] absolute"></div>
+        </div>
+        <div class="w-[1331px] h-[74px] left-[3px] top-0 absolute border border-white">
+            <div class="w-[83.77px] left-[16px] top-[17px] absolute text-white text-[32px] font-light font-['Inter']">標籤
+            </div>
+            <div class="w-[230px] h-[58.07px] left-[100px] top-[7px] absolute">
+                <div
+                    class="w-[230px] h-[58.07px] left-0 top-0 absolute bg-violet-400/opacity-30 rounded-[15px] border border-violet-400">
+                </div>
+                <div
+                    class="w-[133.35px] h-[38px] left-[67px] top-[10px] absolute text-white text-[32px] font-light font-['Inter']">
+                    畢業專題</div>
+            </div>
+            <div class="w-[60px] h-[60px] left-[1257px] top-[7px] absolute">
+                <div class="w-[60px] h-[60px] left-0 top-0 absolute bg-neutral-800 rounded-[100px]"></div>
+            </div>
+        </div>
+    </div>
+</template>
