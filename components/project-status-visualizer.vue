cf76ed553382c6421cd03f1bd9ce0d5616209fce
wen <wenxiangyu523@gmail.com>
2024-08-08 02:44:06 +0800

    change ui

--- components/project-status-visualizer.vue
+++ components/project-status-visualizer.vue
@@ -7,10 +7,10 @@ const props = defineProps({
   <div
     class="flex w-fit items-center justify-center border border-white/50 text-xs font-extralight gap-2 px-4 py-1 rounded-full"
   >
-    <p :class="props.statusId === 1 ? 'font-bold' : ''">成員招募</p>
+    <p :class="props.statusId === 1 ? 'font-bold border-b-white/70 border-b' : ''">成員招募</p>
     <span>></span>
-    <p :class="props.statusId === 2 ? 'font-bold' : ''">指導者招募</p>
+    <p :class="props.statusId === 2 ? 'font-bold border-b-white/70 border-b' : ''">指導者招募</p>
     <span>></span>
-    <p :class="props.statusId === 3 ? 'font-bold' : ''">完成招募</p>
+    <p :class="props.statusId === 3 ? 'font-bold border-b-white/70 border-b' : ''">完成招募</p>
   </div>
 </template>
